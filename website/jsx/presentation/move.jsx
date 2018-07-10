4c3fd8f8dc18e8b4930e75cdcc1f705e97ae88ae
Maarten Nieber <hallomaarten@yahoo.com>
2018-07-10 08:25:59 +0200

    Factor out class VideoLink

--- website/jsx/presentation/move.jsx
+++ website/jsx/presentation/move.jsx
@@ -32,34 +32,50 @@ class Tags extends React.Component {
 }
 
 
+class VideoLink extends React.Component {
+  render() {
+    return (
+      <div className='videolink'>
+        <div className={
+          classnames(
+            'videolink__nrVotes',
+            {
+              'videolink__nrVotes--liked': this.props.item.isLikedByCurrentUser,
+              'videolink__nrVotes--notLiked': !this.props.item.isLikedByCurrentUser,
+            }
+          )
+        }>{this.props.item.nrVotes}</div>
+        <div
+          className={
+            classnames(
+              'videolink__likeBtn',
+              {
+                'videolink__likeBtn--liked': this.props.item.isLikedByCurrentUser,
+                'videolink__likeBtn--notLiked': !this.props.item.isLikedByCurrentUser,
+              }
+            )
+          }
+          onClick={() => this.props.toggleLike(this.props.item.id)}
+        />
+        <a
+          className='videolink__url'
+          href='{this.props.item.url}'
+          target='blank'
+        >
+          {this.props.item.defaultTitle}
+        </a>
+      </div>
+    );
+  }
+}
+
+
+
 class VideoLinkList extends React.Component {
   render() {
     const items = this.props.items.map((item, idx) => {
       return (
-        <div className='videolink' key={idx}>
-          <div className={
-            classnames(
-              'videolink__nrVotes',
-              {
-                'videolink__nrVotes--liked': item.isLikedByCurrentUser,
-                'videolink__nrVotes--notLiked': !item.isLikedByCurrentUser,
-              }
-            )
-          }>{item.nrVotes}</div>
-          <div
-            className={
-              classnames(
-                'videolink__likeBtn',
-                {
-                  'videolink__likeBtn--liked': item.isLikedByCurrentUser,
-                  'videolink__likeBtn--notLiked': !item.isLikedByCurrentUser,
-                }
-              )
-            }
-            onClick={() => this.props.toggleLike(item.id)}
-          />
-          <a className='videolink__url' href='{item.url}' target='blank'>{item.defaultTitle}</a>
-        </div>
+        <VideoLink item={item} key={idx} toggleLike={this.props.toggleLike}/>
       );
     })
 

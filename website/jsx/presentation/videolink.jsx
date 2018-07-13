e8f4c4ba9f5134eca3c7d41094934e1586c69f36
Maarten Nieber <hallomaarten@yahoo.com>
2018-07-13 23:39:47 +0200

    Allow to edit a video link

--- website/jsx/presentation/videolink.jsx
+++ website/jsx/presentation/videolink.jsx
@@ -3,55 +3,144 @@ import classnames from 'classnames';
 
 
 export class VideoLink extends React.Component {
-  render() {
-    return (
-      <div className='videolink'>
-        <div className={
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false
+    }
+  }
+
+  _renderView = () => {
+    const nrVotes = (
+      <div className={
+        classnames(
+          'videolink__nrVotes',
+          {
+            'videolink__nrVotes--voted': this.props.vote != 0,
+            'videolink__nrVotes--notVoted': this.props.vote == 0,
+          }
+        )
+      }>{this.props.item.nrVotes}</div>
+    );
+
+    const upVote = (
+      <div
+        className={
           classnames(
-            'videolink__nrVotes',
+            'videolink__upVoteBtn',
             {
-              'videolink__nrVotes--voted': this.props.vote != 0,
-              'videolink__nrVotes--notVoted': this.props.vote == 0,
+            'videolink__upVoteBtn--voted': this.props.vote == 1,
+            'videolink__upVoteBtn--notVoted': this.props.vote != 1,
             }
           )
-        }>{this.props.item.nrVotes}</div>
-        <div
-          className={
-            classnames(
-              'videolink__upVoteBtn',
-              {
-              'videolink__upVoteBtn--voted': this.props.vote == 1,
-              'videolink__upVoteBtn--notVoted': this.props.vote != 1,
-              }
-            )
-          }
-          onClick={() => {
-            this.props.setVote(this.props.item.id, this.props.vote == 1 ? 0 : 1)
-          }}
-        />
-        <div
-          className={
-            classnames(
-              'videolink__downVoteBtn',
-              {
-              'videolink__downVoteBtn--voted': this.props.vote == -1,
-              'videolink__downVoteBtn--notVoted': this.props.vote != -1,
-              }
-            )
-          }
-          onClick={() => {
-            this.props.setVote(this.props.item.id, this.props.vote == -1 ? 0 : -1)
-          }}
-        />
-        <a
-          className='videolink__url'
-          href={this.props.item.url}
-          target='blank'
-        >
-          {this.props.item.defaultTitle}
-        </a>
+        }
+        onClick={() => {
+          this.props.setVote(this.props.item.id, this.props.vote == 1 ? 0 : 1)
+        }}
+      />
+    );
+
+    const downVote = (
+      <div
+        className={
+          classnames(
+            'videolink__downVoteBtn',
+            {
+            'videolink__downVoteBtn--voted': this.props.vote == -1,
+            'videolink__downVoteBtn--notVoted': this.props.vote != -1,
+            }
+          )
+        }
+        onClick={() => {
+          this.props.setVote(this.props.item.id, this.props.vote == -1 ? 0 : -1)
+        }}
+      />
+    );
+
+    const link = (
+      <a
+        className='videolink__url'
+        href={this.props.item.url}
+        target='blank'
+      >
+        {this.props.item.defaultTitle}
+      </a>
+    );
+
+    const editBtn = (
+      <div
+        className="editVideoLinkBtn ml-2"
+        onClick={() => this.setState({isEditing: true})}
+      >
+      edit
       </div>
     );
+
+    return (
+      <div className='videolink'>
+        {nrVotes}
+        {upVote}
+        {downVote}
+        {link}
+        {editBtn}
+      </div>
+    );
+  }
+
+  _renderEdit = () => {
+    const link = (
+      <input
+        type='text'
+        defaultValue={this.props.item.url}
+        placeholder={'Link'}
+      >
+      </input>
+    );
+
+    const title = (
+      <input
+        type='text'
+        defaultValue={this.props.item.defaultTitle}
+        placeholder={'Description'}
+      >
+      </input>
+    );
+
+    const saveBtn = (
+      <div
+        className="editVideoLinkBtn ml-2"
+        onClick={() => this.setState({isEditing: false})}
+      >
+      save
+      </div>
+    );
+
+    const cancelBtn = (
+      <div
+        className="editVideoLinkBtn ml-2"
+        onClick={() => this.setState({isEditing: false})}
+      >
+      cancel
+      </div>
+    );
+
+    return (
+      <div className='videolink'>
+        {link}
+        {title}
+        {saveBtn}
+        {cancelBtn}
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.isEditing) {
+      return this._renderEdit();
+    }
+    else {
+      return this._renderView();
+    }
   }
 }
 

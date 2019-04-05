36d23302d517c666f5916ccee52340eeb3a6f18d
Maarten Nieber <hallomaarten@yahoo.com>
2019-04-05 15:49:39 +0200

    Apply Prettier

--- website/webpack-common.js
+++ website/webpack-common.js
@@ -1,24 +1,21 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
   entry: {
-    main: './app/main.jsx',
-    app: './app/css/app.jsx',
+    main: "./app/main.jsx",
+    app: "./app/css/app.jsx",
   },
 
   output: {
-    library: 'App'
+    library: "App",
   },
 
   resolve: {
-    modules: [
-      './node_modules',
-      '.',
-    ],
+    modules: ["./node_modules", "."],
     alias: {},
-    extensions: ['*', '.js', '.jsx']
+    extensions: ["*", ".js", ".jsx"],
   },
 
   module: {
@@ -26,47 +23,44 @@ const config = {
       {
         test: /.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: [
-            '@babel/flow',
-            '@babel/preset-react',
-          ],
+          presets: ["@babel/flow", "@babel/preset-react"],
           plugins: [
-            'babel-plugin-rewire',
-            ["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }],
-            '@babel/plugin-proposal-object-rest-spread',
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-proposal-optional-chaining',
-            'syntax-dynamic-import'
-          ]
-        }
+            "babel-plugin-rewire",
+            [
+              "@babel/plugin-proposal-pipeline-operator",
+              { proposal: "minimal" },
+            ],
+            "@babel/plugin-proposal-object-rest-spread",
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-optional-chaining",
+            "syntax-dynamic-import",
+          ],
+        },
       },
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ]
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
-    ]
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: "../css/[name].css",
-      chunkFilename: "../css/[id].css"
-    })
+      chunkFilename: "../css/[id].css",
+    }),
   ],
 };
 
--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,39 +1,41 @@
-var path = require('path');
-var webpack = require('webpack');
-var merge = require('webpack-merge');
-var BundleTracker = require('webpack-bundle-tracker')
-var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
-var common = require('./webpack-common');
-var srvDir = require('./srv-dir');
-
+var path = require("path");
+var webpack = require("webpack");
+var merge = require("webpack-merge");
+var BundleTracker = require("webpack-bundle-tracker");
+var WebpackCleanupPlugin = require("webpack-cleanup-plugin");
+var common = require("./webpack-common");
+var srvDir = require("./srv-dir");
 
 const development = merge(common, {
-  mode: 'development',
+  mode: "development",
 
   output: {
-      path: srvDir + '/static/bundles',
-      filename: "dev-[name]-[hash].js",
-      chunkFilename: '[name].bundle.js',
+    path: srvDir + "/static/bundles",
+    filename: "dev-[name]-[hash].js",
+    chunkFilename: "[name].bundle.js",
   },
 
   plugins: [
     new WebpackCleanupPlugin({
       exclude: ["test.js"],
     }),
-    new BundleTracker({path: srvDir + '/static/bundles', filename: 'webpack-stats.json'})
+    new BundleTracker({
+      path: srvDir + "/static/bundles",
+      filename: "webpack-stats.json",
+    }),
   ],
 });
 
 const test = merge(common, {
-  target: 'web',
-  mode: 'development',
-  entry: ['./test'],
+  target: "web",
+  mode: "development",
+  entry: ["./test"],
   node: {
-    fs: 'empty'
+    fs: "empty",
   },
   output: {
-      path: srvDir + '/static/bundles',
-      filename: "test.js",
+    path: srvDir + "/static/bundles",
+    filename: "test.js",
   },
 });
 

999cfb210294708f4c0645172eb50ef0b96ef95c
Maarten Nieber <hallomaarten@yahoo.com>
2019-09-04 13:47:49 +0200

    fixup! Add request logging middleware

--- django/app/middleware/request_logging/__init__.py
+++ django/app/middleware/request_logging/__init__.py
@@ -4,19 +4,20 @@ import re
 
 def request_logging_middleware(get_response):
     def _log_response(request, response):
-        if request.path.startswith("/api/"):
-            if re.match('^application/json', response.get('Content-Type', ''),
-                        re.I):
-                log_response("RSP", request.method, request.get_full_path(),
-                             response.content, response.status_code)
+        if re.match('^application/json', response.get('Content-Type', ''),
+                    re.I):
+            log_response("RSP", request.method, request.get_full_path(),
+                         response.content, response.status_code)
 
     def middleware(request):
-        if True or request.path.startswith("/api/"):
-            if (request.body):
-                log_request("REQ", request.method, request.get_full_path(),
-                            request.body)
+        if (request.body):
+            log_request("REQ", request.method, request.get_full_path(),
+                        request.body)
 
         response = get_response(request)
+
+        _log_response(request, response)
+
         return response
 
     return middleware

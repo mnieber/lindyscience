929f9b92bcf1a46bb2b2393a524a0f0263b8adde
Maarten Nieber <hallomaarten@yahoo.com>
2019-09-11 20:01:33 +0200

    Force creation of csrftoken in a cookie

--- django/app/views.py
+++ django/app/views.py
@@ -1,3 +1,4 @@
+from django.middleware.csrf import get_token
 from django.views import View
 from django.utils.text import mark_safe
 from django.shortcuts import render
@@ -7,6 +8,10 @@ import json
 
 class AppView(View):
     def get(self, request):
+        # Force inclusion of csrftoken as a cookie
+        csrf_token = get_token(request)
+        assert csrf_token
+
         user_profile = schema.execute(
             '''
             query queryUserProfile {

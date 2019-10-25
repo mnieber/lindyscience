253275370e36516321d324bf007e20a739e31f62
Maarten Nieber <hallomaarten@yahoo.com>
2019-10-25 15:09:44 +0200

    Fix resolve_find_move_lists

--- django/moves/schema/move_list.py
+++ django/moves/schema/move_list.py
@@ -107,14 +107,14 @@ class MoveListQuery(object):
                                 **kwargs):
         result = models.MoveList.objects.filter(
             Q(is_private=False)
-            | Q(owner=info.context.user))
+            | (Q(owner=info.context.user
+                 ) if not info.context.user.is_anonymous else Q()))
 
         if (owner_username):
             result = result.filter(Q(owner__username=owner_username))
 
         if (followed_by_username):
-            profiles = Profile.objects.filter(
-                owner__username=followed_by_username)
+            profiles = Profile.objects.filter(owner__username=followed_by_username)
             if profiles.exists():
                 hits = ProfileToMoveList.objects.filter(profile=profiles[0])
                 result = result.filter(id__in=[x.move_list_id for x in hits])

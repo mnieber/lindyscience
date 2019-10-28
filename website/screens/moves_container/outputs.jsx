dbb5480a33f7227d8b70223dcd163c865304f7ef
Maarten Nieber <hallomaarten@yahoo.com>
2019-10-28 09:07:02 +0100

    SQ use inputs and outputs

---
+++ website/screens/moves_container/outputs.jsx
@@ -0,0 +1,29 @@
+// @flow
+
+import { computed, observable } from "utils/mobx_wrapper";
+import type { UUID } from "kernel/types";
+import { listToItemById } from "utils/utils";
+import { type GetBvrT, facetClass, data } from "facets/index";
+import type { MoveT } from "moves/types";
+
+// $FlowFixMe
+@facetClass
+export class Outputs {
+  @observable preview: Array<MoveT> = [];
+  @data display: Array<MoveT> = [];
+
+  // $FlowFixMe
+  @computed get moveIds(): Array<UUID> {
+    return Object.keys(this.moveById);
+  }
+  // $FlowFixMe
+  @computed get moveById(): { [UUID]: MoveT } {
+    return listToItemById(this.preview);
+  }
+
+  static get: GetBvrT<Outputs>;
+}
+
+export function initOutputs(self: Outputs): Outputs {
+  return self;
+}

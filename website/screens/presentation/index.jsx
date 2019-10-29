92413e3b8efd4a906373a7f2e5cd6625670a9aa5
Maarten Nieber <hallomaarten@yahoo.com>
2019-10-29 10:39:07 +0100

    Remove static divs

--- website/screens/presentation/index.jsx
+++ website/screens/presentation/index.jsx
@@ -14,7 +14,6 @@ import { MoveTable } from "screens/presentation/move_table";
 import { MoveListDetails } from "move_lists/presentation/move_list_details";
 import { CutVideoPanel } from "video/presentation/cut_video_panel";
 import { MovePrivateDataPanel } from "moves/presentation/move_private_data_panel";
-import { StaticTipsPanel } from "tips/presentation/static_tips_panel";
 import { MoveListPlayer } from "screens/presentation/move_list_player";
 import { SearchMovesForm } from "screens/presentation/search_moves_form";
 import {
@@ -37,7 +36,6 @@ const Widgets = {
   MoveListDetails,
   CutVideoPanel,
   MovePrivateDataPanel,
-  StaticTipsPanel,
   SearchMovesForm,
   MoveListFilter,
   MoveListPicker,

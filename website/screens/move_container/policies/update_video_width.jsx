ba6dcd1a8115fddc1ac19a26bf97eabf02ae71cf
Maarten Nieber <hallomaarten@yahoo.com>
2019-11-19 22:49:10 +0100

    Consider screen dimensions when calculating videoWidth

--- website/screens/move_container/policies/update_video_width.jsx
+++ website/screens/move_container/policies/update_video_width.jsx
@@ -6,16 +6,22 @@ import { reaction } from "utils/mobx_wrapper";
 export function updateVideoWidth(ctr: MoveContainer) {
   reaction(
     () => {
+      console.log("WH", window.height);
       const maxVideoWidth = ctr.inputs.sessionDisplay
         ? ctr.inputs.sessionDisplay.maxVideoWidth
         : 100;
       const videoPanelWidth = ctr.display.videoPanelWidth
         ? ctr.display.videoPanelWidth
         : 100;
-      return [maxVideoWidth, videoPanelWidth];
+      const maxWidthDictatedByScreen = (screen.height * 16) / 9;
+      return [maxVideoWidth, videoPanelWidth, maxWidthDictatedByScreen];
     },
-    ([maxVideoWidth, videoPanelWidth]) => {
-      ctr.display.videoWidth = Math.min(maxVideoWidth, videoPanelWidth - 10);
+    ([maxVideoWidth, videoPanelWidth, maxWidthDictatedByScreen]) => {
+      ctr.display.videoWidth = Math.min(
+        maxVideoWidth,
+        videoPanelWidth - 10,
+        maxWidthDictatedByScreen
+      );
     }
   );
 }

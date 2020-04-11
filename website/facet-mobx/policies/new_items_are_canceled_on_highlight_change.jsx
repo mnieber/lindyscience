f0db3e82cc686c3c041c3d59d478121eed79bcb5
Maarten Nieber <hallomaarten@yahoo.com>
2020-04-11 15:37:57 +0200

    Fix: compare on id in newItemsAreCanceledOnHighlightChange

--- website/facet-mobx/policies/new_items_are_canceled_on_highlight_change.jsx
+++ website/facet-mobx/policies/new_items_are_canceled_on_highlight_change.jsx
@@ -4,12 +4,14 @@ import { reaction } from "facet-mobx";
 import { Highlight } from "facet-mobx/facets/highlight";
 import { Addition } from "facet-mobx/facets/addition";
 import { Editing } from "facet-mobx/facets/editing";
+import { path } from "rambda";
 
 export function newItemsAreCanceledOnHighlightChange(ctr: any) {
   reaction(
-    () => Highlight.get(ctr).item,
-    highlightedItem => {
-      if (Addition.get(ctr).item && Addition.get(ctr).item != highlightedItem) {
+    () => Highlight.get(ctr).id,
+    highlightedItemId => {
+      const addedItemId = path("item.id", Addition.get(ctr));
+      if (addedItemId && addedItemId != highlightedItemId) {
         Addition.get(ctr).cancel();
         Editing.get(ctr).setIsEditing(false);
       }

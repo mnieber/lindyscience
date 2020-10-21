c5c8d5060cde04a5681575f8bba5c6fb7d29d0e7
Maarten Nieber <hallomaarten@yahoo.com>
2020-10-21 10:51:37 +0200

    Remove moveIds

--- frontend/src/moves/MovesCtr/facets/Outputs.ts
+++ frontend/src/moves/MovesCtr/facets/Outputs.ts
@@ -1,17 +1,12 @@
 import { computed, observable } from 'src/utils/mobx_wrapper';
 import { MoveT, MoveByIdT } from 'src/moves/types';
-import { UUID } from 'src/kernel/types';
 import { listToItemById } from 'src/utils/utils';
 import { output } from 'facet';
-import { keys } from 'lodash/fp';
 
 export class Outputs {
   @observable preview: Array<MoveT> = [];
   @output display: Array<MoveT> = [];
 
-  @computed get moveIds(): Array<UUID> {
-    return keys(this.moveById);
-  }
   @computed get moveById(): MoveByIdT {
     return listToItemById(this.preview);
   }

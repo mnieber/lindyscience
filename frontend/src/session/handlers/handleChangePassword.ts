e635beda853b64490232492a7e5a8826b4070b87
Maarten Nieber <hallomaarten@yahoo.com>
2020-11-02 13:54:02 +0100

    Move code into index.ts

--- frontend/src/session/handlers/handleChangePassword.ts
+++
@@ -1,5 +0,0 @@
-import * as authApi from 'src/session/apis/authApi';
-
-export const handleChangePassword = (newPassword: string, token: string) => {
-  return authApi.changePassword(newPassword, token);
-};
--- frontend/src/session/handlers/handleResetPassword.ts
+++
@@ -1,5 +0,0 @@
-import * as authApi from 'src/session/apis/authApi';
-
-export const handleResetPassword = async (email: string) => {
-  return await authApi.resetPassword(email);
-};

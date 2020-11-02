44ec5376896bc55aec451516a962ea6e787f48a1
Maarten Nieber <hallomaarten@yahoo.com>
2020-11-02 08:48:56 +0100

    Drop withMoveContextMenu

--- frontend/src/move_lists/containers/MoveListFrame.tsx
+++ frontend/src/move_lists/containers/MoveListFrame.tsx
@@ -1,4 +1,3 @@
-import { compose } from 'lodash/fp';
 import * as React from 'react';
 import { observer } from 'mobx-react';
 import classnames from 'classnames';
@@ -7,31 +6,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVideo } from '@fortawesome/free-solid-svg-icons';
 import { keys } from 'lodash/fp';
 
-import { MoveT } from 'src/moves/types';
-import { Display } from 'src/session/facets/Display';
-import { Profiling } from 'src/session/facets/Profiling';
-import { MoveListT } from 'src/move_lists/types';
-import { UserProfileT } from 'src/profiles/types';
-import { Navigation } from 'src/session/facets/Navigation';
-import { MovesStore } from 'src/moves/MovesStore';
-import { withMoveContextMenu } from 'src/moves/hocs/withMoveContextMenu';
-import { MoveListPlayer } from 'src/move_lists/presentation/MoveListPlayer';
-import { sayMove } from 'src/moves/MovesCtr/handlers/sayMove';
-import { MoveListPicker } from 'src/move_lists/presentation/MoveListPicker';
-import { MoveListHeader } from 'src/move_lists/presentation/MoveListHeader';
-import { MoveListFilter } from 'src/move_lists/presentation/MoveListFilter';
-import { MoveList } from 'src/move_lists/presentation/MoveList';
 import { AccountMenu } from 'src/app/presentation/AccountMenu';
-import { isNone } from 'src/utils/utils';
-import { Selection } from 'facet-mobx/facets/Selection';
-import { Filtering } from 'facet-mobx/facets/Filtering';
 import { Addition } from 'facet-mobx/facets/Addition';
+import { Clipboard } from 'src/moves/MovesCtr/facets/Clipboard';
+import { Display } from 'src/session/facets/Display';
+import { Filtering } from 'facet-mobx/facets/Filtering';
+import { getId } from 'src/app/utils';
+import { isNone } from 'src/utils/utils';
+import { MoveContextMenu } from 'src/moves/presentation/MoveContextMenu';
+import { MoveList } from 'src/move_lists/presentation/MoveList';
+import { MoveListFilter } from 'src/move_lists/presentation/MoveListFilter';
+import { MoveListHeader } from 'src/move_lists/presentation/MoveListHeader';
+import { MoveListPicker } from 'src/move_lists/presentation/MoveListPicker';
+import { MoveListPlayer } from 'src/move_lists/presentation/MoveListPlayer';
+import { MoveListT } from 'src/move_lists/types';
+import { MovesStore } from 'src/moves/MovesStore';
+import { MoveT } from 'src/moves/types';
+import { Navigation } from 'src/session/facets/Navigation';
+import { Profiling } from 'src/session/facets/Profiling';
+import { sayMove } from 'src/moves/MovesCtr/handlers/sayMove';
+import { Selection } from 'facet-mobx/facets/Selection';
 import { useDefaultProps, FC } from 'react-default-props-context';
+import { UserProfileT } from 'src/profiles/types';
 
 type PropsT = React.PropsWithChildren<{}>;
 
 type DefaultPropsT = {
   profiling: Profiling;
+  moveLists: Array<MoveListT>;
   moveList?: MoveListT;
   movesSelection: Selection;
   userProfile?: UserProfileT;
@@ -40,132 +42,144 @@ type DefaultPropsT = {
   navigation: Navigation;
   display: Display;
   movesStore: MovesStore;
-  moveContextMenu: any;
+  movesClipboard: Clipboard;
+  isOwner: (obj: any) => boolean;
 };
 
-export const MoveListFrame: FC<PropsT, DefaultPropsT> = compose(
-  withMoveContextMenu,
-  observer
-)((p: PropsT) => {
-  const props = useDefaultProps<PropsT, DefaultPropsT>(p);
+export const MoveListFrame: FC<PropsT, DefaultPropsT> = observer(
+  (p: PropsT) => {
+    const props = useDefaultProps<PropsT, DefaultPropsT>(p);
 
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const moveListPlayerBtns = (
-    <MoveListPlayer
-      moves={props.movesSelection.items ?? []}
-      sayMove={sayMove}
-      className=""
-    />
-  );
+    const moveListId = getId(props.moveList);
+    const targetMoveLists = props.movesClipboard.targetMoveLists;
 
-  const isFollowing = (ml: MoveListT) =>
-    !!props.userProfile && props.userProfile.moveListIds.includes(ml.id);
+    const targetMoveListsForMoving = props.moveLists.filter(
+      (x: MoveListT) => moveListId !== getId(x)
+    );
 
-  const moveListPicker = (
-    <MoveListPicker
-      key={props.moveList ? props.moveList.id : ''}
-      className=""
-      filter={isFollowing}
-      navigateTo={(x: MoveListT) => props.navigation.navigateToMoveList(x)}
-    />
-  );
-
-  const moveListHeaderBtns = (
-    <MoveListHeader
-      addNewMove={() => {
-        props.movesAddition.add({});
-      }}
-      className="ml-2"
-    />
-  );
-
-  const moveListFilter = (
-    <MoveListFilter
-      className=""
-      moveTags={keys(props.movesStore.tags)}
-      movesFiltering={props.movesFiltering}
-    />
-  );
-
-  const createHostedPanels = (move: MoveT) => {
-    const icon = (
-      <FontAwesomeIcon
-        className={'ml-2 opacity-50'}
-        style={{ marginBottom: '1px' }}
-        icon={faVideo}
-        size="xs"
+    const moveContextMenu = (
+      <MoveContextMenu
+        targetMoveLists={targetMoveLists || []}
+        targetMoveListsForMoving={targetMoveListsForMoving}
+        movesClipboard={props.movesClipboard}
+        isOwnerOfMoveList={!!props.moveList && !!props.isOwner(props.moveList)}
       />
     );
-    return isNone(move.link) || move.link === '' ? undefined : icon;
-  };
 
-  const moveListWidget = (
-    <MoveList
-      className=""
-      createHostedPanels={createHostedPanels}
-      moveContextMenu={props.moveContextMenu}
-      navigateTo={(x: MoveT) => props.navigation.navigateToMove(x)}
-    />
-  );
+    const moveListPlayerBtns = (
+      <MoveListPlayer
+        moves={props.movesSelection.items ?? []}
+        sayMove={sayMove}
+        className=""
+      />
+    );
 
-  const accountMenu = (
-    <div className="pt-4 pb-8 mx-auto">
-      <AccountMenu />
-    </div>
-  );
+    const isFollowing = (ml: MoveListT) =>
+      !!props.userProfile && props.userProfile.moveListIds.includes(ml.id);
 
-  const contents = (
-    <div
-      className={classnames('moveListPanel__inner flexcol', {
-        'moveListPanel__inner--expanded': true,
-      })}
-    >
-      {props.display.small && accountMenu}
-      {moveListPicker}
-      {moveListFilter}
-      <div className="flexrow w-full my-4">
-        {moveListPlayerBtns}
-        {props.moveList &&
-          props.profiling.isOwner(props.moveList) &&
-          moveListHeaderBtns}
+    const moveListPicker = (
+      <MoveListPicker
+        key={props.moveList ? props.moveList.id : ''}
+        className=""
+        filter={isFollowing}
+        navigateTo={(x: MoveListT) => props.navigation.navigateToMoveList(x)}
+      />
+    );
+
+    const moveListHeaderBtns = (
+      <MoveListHeader
+        addNewMove={() => props.movesAddition.add({})}
+        className="ml-2"
+      />
+    );
+
+    const moveListFilter = (
+      <MoveListFilter
+        className=""
+        moveTags={keys(props.movesStore.tags)}
+        movesFiltering={props.movesFiltering}
+      />
+    );
+
+    const createHostedPanels = (move: MoveT) => {
+      const icon = (
+        <FontAwesomeIcon
+          className={'ml-2 opacity-50'}
+          style={{ marginBottom: '1px' }}
+          icon={faVideo}
+          size="xs"
+        />
+      );
+      return isNone(move.link) || move.link === '' ? undefined : icon;
+    };
+
+    const moveListWidget = (
+      <MoveList
+        className=""
+        createHostedPanels={createHostedPanels}
+        moveContextMenu={moveContextMenu}
+        navigateTo={(x: MoveT) => props.navigation.navigateToMove(x)}
+      />
+    );
+
+    const accountMenu = (
+      <div className="pt-4 pb-8 mx-auto">
+        <AccountMenu />
       </div>
-      {moveListWidget}
-    </div>
-  );
+    );
 
-  const shrunkContents = (
-    <CheeseburgerMenu
-      // width="20rem"
-      width={320}
-      isOpen={isMenuOpen}
-      closeCallback={() => setIsMenuOpen(false)}
-    >
-      {contents}
-    </CheeseburgerMenu>
-  );
-
-  const ribbon = (
-    <div
-      className="moveListPanel__ribbon"
-      onClick={() => {
-        setIsMenuOpen(true);
-      }}
-    />
-  );
-
-  return (
-    <div className="MoveListFrame flexrow">
-      {props.display.small ? shrunkContents : contents}
-      {props.display.small && !isMenuOpen && ribbon}
+    const contents = (
       <div
-        className={classnames('MoveListFrame__panel flex-auto', {
-          'pl-4': !props.display.small,
-          'pl-1': props.display.small,
+        className={classnames('moveListPanel__inner flexcol', {
+          'moveListPanel__inner--expanded': true,
         })}
       >
-        {props.children}
+        {props.display.small && accountMenu}
+        {moveListPicker}
+        {moveListFilter}
+        <div className="flexrow w-full my-4">
+          {moveListPlayerBtns}
+          {props.moveList &&
+            props.profiling.isOwner(props.moveList) &&
+            moveListHeaderBtns}
+        </div>
+        {moveListWidget}
       </div>
-    </div>
-  );
-});
+    );
+
+    const shrunkContents = (
+      <CheeseburgerMenu
+        // width="20rem"
+        width={320}
+        isOpen={isMenuOpen}
+        closeCallback={() => setIsMenuOpen(false)}
+      >
+        {contents}
+      </CheeseburgerMenu>
+    );
+
+    const ribbon = (
+      <div
+        className="moveListPanel__ribbon"
+        onClick={() => setIsMenuOpen(true)}
+      />
+    );
+
+    return (
+      <div className="MoveListFrame flexrow">
+        {props.display.small ? shrunkContents : contents}
+        {props.display.small && !isMenuOpen && ribbon}
+        <div
+          className={classnames('MoveListFrame__panel flex-auto', {
+            'pl-4': !props.display.small,
+            'pl-1': props.display.small,
+          })}
+        >
+          {props.children}
+        </div>
+      </div>
+    );
+  }
+);

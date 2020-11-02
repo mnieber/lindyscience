fde4fdf745f512e1c88bd4f7d9d6adcf8daaa87d
Maarten Nieber <hallomaarten@yahoo.com>
2020-11-02 11:07:41 +0100

    Simplify CutVideoPanel

--- frontend/src/video/presentation/CutVideoPanel.tsx
+++ frontend/src/video/presentation/CutVideoPanel.tsx
@@ -1,88 +1,90 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
+import { useDefaultProps, FC } from 'react-default-props-context';
+import { keys } from 'lodash/fp';
 
 import { UUID } from 'src/kernel/types';
-import { Display as SessionDisplay } from 'src/session/facets/Display';
-import { Display as MoveDisplay } from 'src/moves/MoveCtr/facets/Display';
-import { TagT } from 'src/tags/types';
 import { CutPoints } from 'src/video/facets/CutPoints';
 import { VideoPlayerPanel } from 'src/video/presentation/VideoPlayerPanel';
 import { CutPointList } from 'src/video/presentation/CutPointList';
+import { MovesStore } from 'src/moves/MovesStore';
 
-type PropsT = {
-  moveTags: Array<TagT>;
+type PropsT = {};
+
+type DefaultPropsT = {
   cutPoints: CutPoints;
-  sessionDisplay: SessionDisplay;
-  moveDisplay: MoveDisplay;
+  movesStore: MovesStore;
 };
 
-export const CutVideoPanel: React.FC<PropsT> = observer((props: PropsT) => {
-  const onKeyDown = (e: any) => {
-    if (e.keyCode === 13) {
-      props.cutPoints.setVideoLink(e.target.value);
-    }
-  };
+export const CutVideoPanel: FC<PropsT, DefaultPropsT> = observer(
+  (p: PropsT) => {
+    const props = useDefaultProps<PropsT, DefaultPropsT>(p);
 
-  const linkPanel = (
-    <div className="flexrow h-8">
-      <input
-        id="linkPanelInput"
-        className="w-full"
-        onKeyDown={onKeyDown}
-        placeholder="Video link"
-      />
-    </div>
-  );
+    const onKeyDown = (e: any) => {
+      if (e.keyCode === 13) {
+        props.cutPoints.setVideoLink(e.target.value);
+      }
+    };
 
-  const videoPlayerPanel = (
-    <VideoPlayerPanel
-      key="videoPlayerPanel"
-      videoController={props.cutPoints.videoController}
-      sessionDisplay={props.sessionDisplay}
-      moveDisplay={props.moveDisplay}
-    />
-  );
-
-  const selectCutPointById = (id: UUID, isShift: boolean, isCtrl: boolean) =>
-    undefined;
-
-  const cutPointList = (
-    <CutPointList
-      moveTags={props.moveTags}
-      cutPoints={props.cutPoints}
-      highlightedCutPoint={undefined}
-      selectCutPointById={selectCutPointById}
-    />
-  );
-
-  const buttonCreateMoves = (
-    <div className="mt-4">
-      <button
-        className="button"
-        onClick={() => {
-          props.cutPoints.createMoves();
-          props.cutPoints.remove(
-            (props.cutPoints.cutPoints ?? []).map((x) => x.id)
-          );
-        }}
-      >
-        Create moves
-      </button>
-    </div>
-  );
-  return (
-    <div>
-      <h2 className="mt-4">Cut videos</h2>
-      <div className={'cutVideoPanel panel'}>
-        <p className="mb-2">
-          This section is used for creating one or more new moves from a single
-          video.
-        </p>
-        {linkPanel}
-        {videoPlayerPanel}
-        {buttonCreateMoves}
-        {cutPointList}
+    const linkPanel = (
+      <div className="flexrow h-8">
+        <input
+          id="linkPanelInput"
+          className="w-full"
+          onKeyDown={onKeyDown}
+          placeholder="Video link"
+        />
       </div>
-    </div>
-  );
-});
+    );
+
+    const videoPlayerPanel = (
+      <VideoPlayerPanel
+        key="videoPlayerPanel"
+        videoController={props.cutPoints.videoController}
+      />
+    );
+
+    const selectCutPointById = (id: UUID, isShift: boolean, isCtrl: boolean) =>
+      undefined;
+
+    const cutPointList = (
+      <CutPointList
+        moveTags={keys(props.movesStore.tags)}
+        cutPoints={props.cutPoints}
+        highlightedCutPoint={undefined}
+        selectCutPointById={selectCutPointById}
+      />
+    );
+
+    const buttonCreateMoves = (
+      <div className="mt-4">
+        <button
+          className="button"
+          onClick={() => {
+            props.cutPoints.createMoves();
+            props.cutPoints.remove(
+              (props.cutPoints.cutPoints ?? []).map((x) => x.id)
+            );
+          }}
+        >
+          Create moves
+        </button>
+      </div>
+    );
+    return (
+      <div>
+        <h2 className="mt-4">Cut videos</h2>
+        <div className={'cutVideoPanel panel'}>
+          <p className="mb-2">
+            This section is used for creating one or more new moves from a
+            single video.
+          </p>
+          {linkPanel}
+          {videoPlayerPanel}
+          {buttonCreateMoves}
+          {cutPointList}
+        </div>
+      </div>
+    );
+  }
+);

f1b6924b3e06df97437f577ed0a3649bbc2c6538
Maarten Nieber <hallomaarten@yahoo.com>
2020-11-11 01:17:16 +0100

    Fix type of this pointer

--- frontend/src/session/handlers/handleAddMoveListTags.ts
+++ frontend/src/session/handlers/handleAddMoveListTags.ts
@@ -1,12 +1,12 @@
 import { values } from 'lodash/fp';
 
 import { MoveListByIdT } from 'src/move_lists/types';
-import { MovesStore } from 'src/moves/MovesStore';
+import { MoveListsStore } from 'src/move_lists/MoveListsStore';
 import { SessionContainer } from 'src/session/SessionCtr';
 import { getCtr } from 'facet';
 
 export function handleAddMoveListTags(
-  this: MovesStore,
+  this: MoveListsStore,
   moveListById: MoveListByIdT
 ) {
   const ctr = getCtr<SessionContainer>(this);
